7ea9393f711ecaaae505eb421d9f81d0dfcaa08a
Jamie097 <jakubkylar13@icloud.com>
2023-07-04 11:32:31 +0200

    Initial Commit

---
+++ SpaceXLaunch/SpaceXLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXLaunchApp.swift
+//  SpaceXLaunch
+//
+//  Created by Jakub on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
